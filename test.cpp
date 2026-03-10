6239d15aca0579ace11705b570e0848c182c3136
rladmsghtkfkdgo <hearang3180@naver.com>
2026-03-11 00:37:35 +0900

    pandas-ready

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // 입출력을 위한 라이브러리 포함
+
+int main() {
+    // std::cout은 출력을, std::endl은 줄바꿈을 의미합니다.
+    std::cout << "Hello, Minkyung!" << std::endl;
+    std::cout << "C++ starts now!" << std::endl;
+    
+    return 0; // 프로그램이 성공적으로 종료되었음을 알림
+}
---
+++ test.py
@@ -0,0 +1,4 @@
+
+print("판다스버젼:", pandas.__version__)
+print("넘파이버젼:", numpy.__version__)
+print("Success.")
